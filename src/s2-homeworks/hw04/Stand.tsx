2316a9df3a043b32c2664656e5879658ab56ef39
Neakhai Yauheni <beonezero@protonmail.com>
2023-06-10 22:15:36 +0300

    hw4

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -50,7 +50,7 @@ const Stand = () => {
                 {/*красная кнопка:*/}
                 <div>
                     <SuperButton id={'hw4-super-button-red'} xType={'red'}>
-                        Опасность
+                        Красная
                     </SuperButton>
                 </div>
                 {/*задизэйбленная кнопка:*/}
