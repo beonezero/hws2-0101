8cea38f76625131c6c622adcf7c21d07f2208c5c
Neakhai Yauheni <beonezero@protonmail.com>
2023-06-22 23:20:38 +0300

    hw8 finish

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,36 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            const copyState = [...state]
+            if(action.payload === "up") {
+                return copyState.sort(function(a,b) {
+                   if (a.name > b.name){
+                       return 1
+                   } else if (a.name < b.name) {
+                       return -1
+                   } else {
+                       return 0
+                   }
+                })
+            }
+            if(action.payload === "down") {
+                return copyState.sort(function(a,b) {
+                    if (a.name < b.name){
+                        return 1
+                    } else if (a.name > b.name) {
+                        return -1
+                    } else {
+                        return 0
+                    }
+                })
+            }
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
