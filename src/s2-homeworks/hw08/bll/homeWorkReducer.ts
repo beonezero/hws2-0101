b07a6a1e510fbb92f2e7a2882f4e2d81e58fe161
Neakhai Yauheni <beonezero@protonmail.com>
2023-06-23 00:05:37 +0300

    hw8 usereducer refactor

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,30 +7,15 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': {
-            const copyState = [...state]
-            if(action.payload === "up") {
-                return copyState.sort(function(a,b) {
-                   if (a.name > b.name){
-                       return 1
-                   } else if (a.name < b.name) {
-                       return -1
-                   } else {
-                       return 0
-                   }
-                })
-            }
-            if(action.payload === "down") {
-                return copyState.sort(function(a,b) {
-                    if (a.name < b.name){
-                        return 1
-                    } else if (a.name > b.name) {
-                        return -1
-                    } else {
-                        return 0
-                    }
-                })
-            }
-            return state
+            return [...state].sort((a,b) => {
+                   if (action.payload === 'up'){
+                       if (a.name > b.name) {return 1}
+                       if (a.name < b.name) {return -1}
+                   } else if (action.payload === 'down') {
+                       if (a.name < b.name) {return 1}
+                       if (a.name > b.name) {return -1}
+                   } return 0
+                } )
         }
         case 'check': {
             return state.filter(u => u.age >= action.payload)
